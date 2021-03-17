871171f236dffb90cdff87489808fa4284ec1997
amaljacob <1114778@apps.nsd.org>
2021-03-17 00:08:19 -0700

    Finished optimized solution, pruning dictionary for each phrase

--- src/amal/AnagramSolver.java
+++ src/amal/AnagramSolver.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public class AnagramSolver {
+	HashMap<String, LetterInventory> backupDictionary;
 	HashMap<String, LetterInventory> dictionary;
 
 	/**
@@ -24,6 +25,9 @@ public class AnagramSolver {
 		for (int i = 0; i < list.size(); i++) {
 			dictionary.put(list.get(i), new LetterInventory(list.get(i)));
 		}		
+		
+		//prepares backup dictionary to reset dictionary after pruning
+		backupDictionary = (HashMap<String, LetterInventory>) dictionary.clone();
 	}
 	
 	/**
@@ -49,29 +53,68 @@ public class AnagramSolver {
 		//makes letter inventory for string to be analyzed
 		LetterInventory stringLetters = new LetterInventory(s);
 		
+	    //as recursion progresses, this ArrayList will build up words in potential anagrams
 		ArrayList<String> matchingWords = new ArrayList<String>();
+		
+		//prunes dictionary to speed up traversing in the recursive helper method
+		pruneDictionary(stringLetters);
 		
 		//recursive helper method
 		printHelper(stringLetters, max, matchingWords);
 
-		
+		//resets the dictionary
+		dictionary = (HashMap<String, LetterInventory>) backupDictionary.clone();
 		
 	}
 	
+	/**
+	 * This method will prune the dictionary to only include words that can possibly be created
+	 * from the given string. This ensures that we do not traverse through these words unnecessarily
+	 * and multiple times during our recursion
+	 * 
+	 * @param stringLetters The LetterInventory of the string that we are building anagrams from
+	 */
+	public void pruneDictionary(LetterInventory stringLetters) {
+		// Create a Iterator to KeySet of HashMap
+		Iterator<String> it = dictionary.keySet().iterator();
+		// Iterate over all the dictionary words
+		while (it.hasNext()) {
+		    String key = it.next();
+		    
+		    //if there isn't enough letters to make this word, get rid of it from our dictionary
+			if (stringLetters.subtract(dictionary.get(key)) == null) {
+				it.remove();
+			}
+		}
+	}
+	
+	
+	/**
+	 * This helper method will recursively build combinations of words that form anagrams using the given string
+	 * 
+	 * @param stringLetters The LetterInventory of the string we are building anagrams from
+	 * @param max The maximum number of words that can be used, given by the user
+	 * @param matchingWords Our collection bucket for building a combination of words that form an anagram
+	 */
 	public void printHelper(LetterInventory stringLetters, int max, ArrayList<String> matchingWords) {
+		//first check if we are done and can print
 		if (stringLetters.size() == 0) {
 			System.out.print(matchingWords);
+		//if not, check whether we have used too many words and need to stop
 		} else if (max <= 0) {
 			return;
 		} 
+		//traverse through all possible words in the dictionary, and if they can be made with the current 
+		//amount of letters, repeat the entire method with the remaining letters and add the word to matchingWords
 		for (Map.Entry<String, LetterInventory> entry : dictionary.entrySet()) {
 			if (stringLetters.subtract(entry.getValue()) != null) {
 				matchingWords.add(entry.getKey());
 				printHelper(stringLetters.subtract(entry.getValue()), max - 1, matchingWords);
+				
+				//delete the word in order to refresh matchingWords for the next iteration
 				matchingWords.remove(entry.getKey());
 				//repeat our iteration
 			}
-		    //System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
 		}
 		
 	}
